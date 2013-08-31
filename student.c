e9b01cb2120f7056fb8b6a5bf2a42d68436d989c
Kerry Xing <kxing@mit.edu>
2013-08-31 09:19:41 -0400

    Change in the student's code.

--- student.c
+++ student.c
@@ -1,9 +1,16 @@
 #include "student.h"
 
+/**
+ * Takes the square of an integer. Assumes that the result will fit into an int.
+ */
 int square(int x) {
   return x * x;
 }
 
+/**
+ * Takes in an integer array and its length, and returns the sum of the elements
+ * of the array. Assumes that the result will fit into an int.
+ */
 int sum(int* array, int length) {
   int running_total = 0;
 
